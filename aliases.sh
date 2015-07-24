febe05596e73b6b432afa4abc94eb5efd5c15fd1
Gocho Mugo <mugo@forfuture.co.ke>
2015-07-24 13:50:59 +0300

    init

---
+++ aliases.sh
@@ -0,0 +1,15 @@
+#
+# my awesome aliases
+#
+
+
+# npm
+alias npm.g="msu run submarine.npm.g"
+alias npm.ginstalled="msu run submarine.npm.ginstalled"
+alias npm.gln="msu run submarine.npm.gln"
+alias npm.gremove="msu run submarine.npm.gremove"
+alias npm.grestore="msu run submarine.npm.grestore"
+alias npm.gtrack="msu run submarine.npm.gtrack"
+alias npm.gupdate="msu run submarine.npm.gupdate"
+alias npm.ln="msu run submarine.npm.ln_mod"
+alias npm.ln_bin="msu run submarine.npm.ln_bin"
---
+++ readme.md
@@ -0,0 +1,21 @@
+
+# submarine
+
+> **I want a Submarine, mum**
+
+[![Terminal Lover](https://img.shields.io/badge/terminal-lover-red.svg?style=flat-square)](https://github.com/GochoMugo)
+
+
+## installation:
+
+Ensure you have installed [msu](https://github.com/GochoMugo/msu):
+
+```bash
+⇒ msu install gh:GochoMugo/submarine
+```
+
+## license:
+
+__The MIT License (MIT)__
+
+Copyright (c) 2015 GochoMugo <mugo@forfuture.co.ke>
