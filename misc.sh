297ef536180ca54c1770903199923dd97ce2a122
GochoMugo <mugo@forfuture.co.ke>
2015-10-29 16:29:54 +0300

    (misc) Add extract function

---
+++ misc.sh
@@ -0,0 +1,34 @@
+#
+# Miscellaneous operations
+#
+# References:
+# * https://github.com/alex-cory/fasthacks
+#
+
+
+# modules
+msu_require "console"
+
+
+# extracting archives
+function extract() {
+  if [ -f "$1" ] ; then
+    case $1 in
+      *.tar.bz2)   tar xvjf "$1"     ;;
+      *.tar.gz)    tar xvzf "$1"     ;;
+      *.bz2)       bunzip2 "$1"      ;;
+      *.rar)       unrar x "$1"      ;;
+      *.gz)        gunzip "$1"       ;;
+      *.tar)       tar xvf "$1"      ;;
+      *.tbz2)      tar xvjf "$1"     ;;
+      *.tgz)       tar xvzf "$1"     ;;
+      *.zip)       unzip "$1"        ;;
+      *.Z)         uncompress "$1"   ;;
+      *.7z)        7z x "$1"         ;;
+      *)           error "'$1' cannot be extracted via >extract<" ;;
+    esac
+  else
+    error "'$1' is not a valid file!"
+  fi
+}
+
